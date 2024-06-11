07796200b83b0a92017df9db9264d8885f145973
LaggingBehind <ascelite@hotmail.com>
2024-06-10 23:33:25 -0500

    Toppings

---
+++ menu.py
@@ -0,0 +1,9 @@
+import time
+
+def hello_message():
+    print("Welcome to Cheesus Crust Pizza!")
+    print("How may we serve thee?")
+    pizza_topping()
+    
+def pizza_topping():
+    toppings = ["Mushrooms","Sausage"]
