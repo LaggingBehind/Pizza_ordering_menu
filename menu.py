baa0400e99b7c47be58f2e05c20c923f7861af6e
LaggingBehind <ascelite@hotmail.com>
2024-06-13 15:58:29 -0500

    size_numbers

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from os import name
+import select
 import time
 
 order_list = []
@@ -30,6 +31,24 @@ total_cost = 0
 def hello_message():
   print("Welcome to Cheesus Crust Pizza!")
   print("How may we serve thee?")
+  
+def choose_pizza_size():
+  global total_cost
+  print("Please select the size of your pizza:")
+  for key, value in pizza_menu.items():
+    print(f"{key}.{value['name']}-${value['price']:.2f}")
+    
+  while true:
+    try:
+      size_choice=int(input("Enter the number corresponding to your choice:"))
+      if size_choice in pizza_menu:
+        select_pizza=pizza_menu[size_choice]
+        total_cost+=selected_pizza["price"]
+        order_list.append(selected_pizza["name"])
+        break
+      else:print("Invalid selection. Please choose a menu item number")
+  exept ValueError:
+    print("Invalid, Please enter a number.")
 
 
 def pizza_topping():
